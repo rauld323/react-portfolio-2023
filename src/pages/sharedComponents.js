2badb65bfefbb58fce5b0add599c3835e49e95a6
Raul <rauldavila323@gmail.com>
2023-08-05 14:00:32 +0200

    change folder structure

--- src/pages/sharedComponents.js
+++ src/pages/sharedComponents.js
@@ -1,4 +1,4 @@
 export { default as Header } from '../components/Header/Header';
 export { default as Intro } from '../pages/Intro';
 export { default as Details } from '../pages/Details';
-export { default as Projects } from '../pages/Projects';
+export { default as Projects } from '../components/Projects/Projects';
