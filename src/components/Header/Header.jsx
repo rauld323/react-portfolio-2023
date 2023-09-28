9b8b8b7354a55a304ab7bae728b87140cfe108b2
Raul Davila <rauldavila323@gmail.com>
2023-09-28 14:06:29 +0200

    remover header css file

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,30 @@
-import styles from "./Header.module.css";
+import styled from "styled-components";
 
 const Header = () => {
   return (
-    <header className={styles.logo}>
-      <a className={styles.logoText} href="#Intro">
-        Raul Davila Campos
-      </a>
-    </header>
+    <StyledHeader>
+      <StyledAnchorText href="#Intro">Raul Davila Campos</StyledAnchorText>
+    </StyledHeader>
   );
 };
 
 export default Header;
+
+const StyledHeader = styled.header`
+  height: 50px;
+  width: 100%;
+  overflow: hidden;
+  z-index: 1;
+  display: flex;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  background-color: white;
+`;
+
+const StyledAnchorText = styled.a`
+  float: left;
+  color: black;
+  padding: 0px 16px;
+  text-decoration: none;
+`;
